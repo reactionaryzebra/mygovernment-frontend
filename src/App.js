1abbfabe389f2d2384d2c16059275eac379efb0d
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-13 16:02:29 -0700

    Add RepList component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import * as routes from "./constants/routes";
+import MyRepresentatives from "./components/Home/RepList";
 import Auth from "./components/Auth/Auth";
 import Landing from "./components/Landing/Landing";
 import "./App.css";
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path={routes.HOME} render={() => <Auth />} />
+        <Route exact path={routes.HOME} render={() => <MyRepresentatives />} />
         <Route exact path={routes.ROOT} render={() => <Landing />} />
       </Switch>
     </div>
---
+++ src/components/Home/RepList.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const MyRepresentatives = () => (
+  <div>
+    <div>
+      <h3>Federal</h3>
+    </div>
+    <div />
+    <div>
+      <h3>State</h3>
+    </div>
+    <div />
+    <div>
+      <h3>Local</h3>
+    </div>
+    <div />
+  </div>
+);
+
+export default MyRepresentatives;
