688e8a0061fda0a28615cb2b83dd2d124ca56343
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-14 12:25:11 -0700

    Add logic to split representatives into levels of gov

--- src/components/Home/RepList.jsx
+++ src/components/Home/RepList.jsx
@@ -1,55 +1,93 @@
 import React, { useState, useEffect } from "react";
 
 const MyRepresentatives = ({ address }) => {
-  const [repList, setRepList] = useState([]);
+  const [federalReps, setFederalReps] = useState([]);
+  const [stateReps, setStateReps] = useState([]);
+  const [localReps, setLocalReps] = useState([]);
 
   const query = `
     query Representative($address: String!){
       representatives(address: $address) {
         name
         office
+        division
       }
     }
   `;
 
   const variables = { address: address };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch("http://localhost:9000/graphql", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ query, variables })
-      });
-      const parsedData = await data.json();
-      setRepList(parsedData.data.representatives);
-    };
-    fetchData();
-  }, []);
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const data = await fetch("http://localhost:9000/graphql", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ query, variables })
+        });
+        const parsedData = await data.json();
+        setFederalReps(
+          parsedData.data.representatives.filter(rep =>
+            rep.office.includes("United States")
+          )
+        );
+        setStateReps(
+          parsedData.data.representatives.filter(
+            rep => rep.division.includes('state') && !rep.office.includes('United States') && !rep.division.includes('county') && !rep.division.includes('place')
+          )
+        );
+        setLocalReps(
+          parsedData.data.representatives.filter(
+            rep =>
+              rep.division.includes("place") || rep.division.includes("county")
+          )
+        );
+      };
+      fetchData();
+    },
+    []
+  );
 
   return (
     <div>
       <div>
         <h3>Federal</h3>
+      </div>
+      <div>
         <ul>
-          {repList.map(rep => (
-            <li>
+          {federalReps.map((rep, i) => (
+            <li key={i}>
               {rep.office} - {rep.name}
             </li>
           ))}
         </ul>
       </div>
-      <div />
       <div>
         <h3>State</h3>
       </div>
-      <div />
+      <div>
+        <ul>
+          {stateReps.map((rep, i) => (
+            <li key={i}>
+              {rep.office} - {rep.name}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div>
         <h3>Local</h3>
       </div>
-      <div />
+      <div>
+        <ul>
+          {localReps.map((rep, i) => (
+            <li key={i}>
+              {rep.office} - {rep.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
