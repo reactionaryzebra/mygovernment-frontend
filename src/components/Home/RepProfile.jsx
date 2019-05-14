4413318ddc254a8ac7ca7da8c7998b4b1bf28a86
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-14 16:42:27 -0700

    Add all fields to RepProfile

--- src/components/Home/RepProfile.jsx
+++ src/components/Home/RepProfile.jsx
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 const RepProfile = ({ currentRep, address }) => {
+
+  const [rep, setRep] = useState({})
+
   const query = `
     query Representative($address:String!, $name: String!){
       representative(address: $address, name: $name) {
         name
         office
-        division
+        party
+        photoUrl
+        news {
+          url
+          title
+          description
+        }
+        bills {
+          short_title
+          summary
+        }
+        committees {
+          name
+          title
+        }
       }
     }
   `;
@@ -22,12 +39,65 @@ const RepProfile = ({ currentRep, address }) => {
         body: JSON.stringify({ query, variables })
       });
       const parsedData = await data.json();
-      console.log(parsedData);
+      setRep(parsedData.data.representative)
     };
     fetchData();
-  });
+  }, []);
 
-  return <div />;
+  return <div>
+    <img src={rep.photoUrl} style={{width: '100px', height:'100px'}} />
+    <h2>{rep.name}</h2>
+    <h3>{rep.office}</h3>
+    <h4>{rep.party}</h4>
+    <div>
+      <h4>Committees</h4>
+      <div>
+        <ul>
+          {rep.committees ? rep.committees.map((committee, i) => <li>
+            <div>
+              <h5>
+                {committee.title} - {committee.name}
+              </h5>
+            </div>
+          </li>) : null}
+        </ul>
+      </div>
+    </div>
+    <div>
+      <h4>Bills</h4>
+      <div>
+        <ul>
+          {rep.bills ? rep.bills.map((bill, i) => <li>
+            <div>
+              <h5>
+                {bill.short_title}
+              </h5>
+              <p>
+                {bill.summary}
+              </p>
+            </div>
+          </li>) : null}
+        </ul>
+      </div>
+    </div>
+    <div>
+      <h4>
+        News
+      </h4>
+      <div>
+        <ul>
+          {rep.news ? rep.news.map((article, i) =>
+            <li key={i}>
+              <div>
+                <h5><a href={article.url}>{article.title}</a></h5>
+                <p>{article.description}</p>
+              </div>
+            </li>
+          ) : null}
+        </ul>
+      </div>
+    </div>
+  </div>;
 };
 
 export default RepProfile;
