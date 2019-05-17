e1ef3c8bd0ad7f178871e02300fb5451f9b03938
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-16 18:48:31 -0700

    Style profile

---
+++ src/styles/Profile.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Profile = styled.div`
+  overflow: auto;
+  width: 65vw;
+  height: 90vh;
+  background-color: rgba(255,255,255, 0.9);
+  box-shadow: 0px 0px 3px 5px rgba(255,255,255,0.9);
+  margin-right: 1vw;
+  .header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .header div {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export default Profile;
